773921f7adf2fd489095e62fc1557dc4b2063117
Bogdan Pushkarskiy <fosker3@gmail.com>
2016-09-15 16:08:47 +0300

    counter

--- public_html/app/js/counters.js
+++ public_html/app/js/counters.js
@@ -11,23 +11,34 @@ $.fn.isVisible = function() {
 
 function doCheck() {
 
-    var options = {
-        useEasing : true,
-        useGrouping: true,
-        separator : ',',
-        decimal : '.',
-        prefix : '',
-        suffix : ''
-    };
+    $('.counter').css("visibility", "hidden");
 
-    var counters = $('.counter');
+    setTimeout(function () {
+        var easingFn = function (t, b, c, d) {
+            var ts = (t /= d) * t;
+            var tc = ts * t;
+            return b + c * (tc + -3 * ts + 3 * t);
+        };
+        var options = {
+            useEasing : true,
+            easingFn: easingFn,
+            useGrouping : true,
+            separator : ',',
+            decimal : '.',
+            prefix : '',
+            suffix : ''
+        };
 
-    $.each(counters, function() {
-        if($(this).isVisible()) {
-            var counter = new CountUp(this, 0, $(this).text(), 0, 5, options);
-            counter.start(afterCount(this));
-        }
-    });
+        var counters = $('.counter');
+        counters.css("visibility", "visible");
+
+        $.each(counters, function() {
+            if($(this).isVisible()) {
+                var counter = new CountUp(this, 0, $(this).text(), 0, 5, options);
+                counter.start(afterCount(this));
+            }
+        });
+    }, 1500);
 }
 
 function afterCount(item) {
